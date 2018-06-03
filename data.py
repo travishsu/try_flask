54c47e7fb831db55b32a22468dd87884a53ca3f4
Yichung Hsu <fov223@gmail.com>
2018-06-04 07:31:25 +0800

    to github

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'adskjaksjd qwjkekqwj fvuiiuqwnbfv ajksj kqwi oq ajskdjk, saj qiw',
+            'author': 'Some One',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'adskjaksjd qwjkekqwj fvuiiuqwnbfv ajksj kqwi oq ajskdjk, saj qiw',
+            'author': 'Some Two',
+            'create_date': '04-26-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'adskjaksjd qwjkekqwj fvuiiuqwnbfv ajksj kqwi oq ajskdjk, saj qiw',
+            'author': 'Some Three',
+            'create_date': '04-27-2017'
+        },
+    ]
+    return articles
